044a70f0c6cd20bf184ee78331aefc4ee10e0415
Ferysuryanta <ferysuryanta01@gmail.com>
2023-04-18 21:34:31 +0700

    changes sql

--- sql/dans_multi_pro_db.sql
+++ sql/dans_multi_pro_db.sql
@@ -33,9 +33,9 @@ CREATE TABLE `users` (
 /*Data for the table `users` */
 
 insert  into `users`(`id`,`name`,`password`,`role`,`email`) values 
-(1,'hikari','$2a$10$tCWTi55rVE3T9bNVpAPQoONcK6zgb7iMeDgb49imXvTZON5GUo/bG','ROLE_USER','hikari@gmail.com'),
-(2,'hikari29','$2a$10$utvyhuCpvYqU5do6aXB5U.vtAQLeZf7J/rzu5WKy26an.aW9WwVoW','ROLE_USER','hikari29@gmail.com'),
-(3,'hikari2','$2a$10$xeZNZCwJ3kKKbPaVVnqACePFqZcIRIomKK1B3tK76N26YACTDzEBW','ROLE_USER','hikari2@gmail.com');
+(1,'Test','$2a$10$tCWTi55rVE3T9bNVpAPQoONcK6zgb7iMeDgb49imXvTZON5GUo/bG','ROLE_USER','Test@gmail.com'),
+(2,'TestApp','$2a$10$utvyhuCpvYqU5do6aXB5U.vtAQLeZf7J/rzu5WKy26an.aW9WwVoW','ROLE_USER','TestApp@gmail.com'),
+(3,'TestApp1','$2a$10$xeZNZCwJ3kKKbPaVVnqACePFqZcIRIomKK1B3tK76N26YACTDzEBW','ROLE_USER','TestApp1@gmail.com');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
